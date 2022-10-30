7ad75aa3584fba447761799779f12f0e4ff831cc
Andrey Kurnosov <dead_god.666@icloud.com>
2022-10-31 02:37:58 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+const swiper = new Swiper();
+*/
+
+
+const iconMenu = document.querySelector('.icon-menu');
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu__body');
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle("_lock");
+		iconMenu.classList.toggle("_active");
+		menuBody.classList.toggle("_active");
+	});
+}
